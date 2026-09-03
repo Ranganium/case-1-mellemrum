430355f2ef75736a52d82e635b1f5524feb39c89
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-09-03 11:05:23 +0200

    Der bliver nu scrollet til toppen af hver side nu
    Der er blevet lavet et ScrollToTop component. Proppen er sat ind på main.jsx, hvor nu gør så alle side scroller til toppen

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles.css";
 import App from "./App.jsx";
+import ScrollToTop from "./components/ScrollToTop.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
